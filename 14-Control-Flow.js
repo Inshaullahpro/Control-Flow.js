5793bffbd3f5a973eb3cce77da12d197423667f2
Inshaullahpro <kundiinshaullah3@gmail.com>
2025-07-29 00:32:37 +0500

    Update 14-Control-Flow.js

--- 14-Control-Flow.js
+++ 14-Control-Flow.js
@@ -1,73 +1,73 @@
- ///       if 
+ /       if 
 
-//  const isUserloggidIn  = true;
-//  const temperature = 41;
+ const isUserloggidIn  = true;
+ const temperature = 41;
 
-//  if  (temperature === 50) {
-//     console.log("less than 50");
+ if  (temperature === 50) {
+    console.log("less than 50");
     
-//  } else {
-//     console.log("temperature is greater is than 50");
+ } else {
+    console.log("temperature is greater is than 50");
     
-//  }
-//  console.log("execute");
+ }
+ console.log("execute");
  
 
 
  // <, >, <=, >=, ==, !=, ===
 
 
-//  const score  =200;
+ const score  =200;
 
-//  if (score > 100) {
-//     let power  = "fly"; 
-//     console.log(`user power:  ${power}`);
-//  }
-//    console.log(`user power:  ${power}`);
+ if (score > 100) {
+    let power  = "fly"; 
+    console.log(`user power:  ${power}`);
+ }
+   console.log(`user power:  ${power}`);
 
 
 const balance  = 1000;
-// if (balance > 500) console.log("test"),console.log("test2")
+if (balance > 500) console.log("test"),console.log("test2")
 
-// if (balance > 500) {
-//     console.log("less tan 500");   
-// } else if (balance < 750) {
-//     console.log("less than 750");
+if (balance > 500) {
+    console.log("less tan 500");   
+} else if (balance < 750) {
+    console.log("less than 750");
 
-// } else if (balance > 900) {
-//     console.log("less than 750");
-// }  else  {
-//     console.log("less than 1200")
-// }
+} else if (balance > 900) {
+    console.log("less than 750");
+}  else  {
+    console.log("less than 1200")
+}
 
 
-// const userLoggedIn = true;
-// const debitCard = true;
-// const LoggedInFormGoggle = false;
-// const LoggedInFormEmail  = true;
-// if (userLoggedIn && debitCard && 2 == 2) {
-//     console.log("Allow to by courses");
+const userLoggedIn = true;
+const debitCard = true;
+const LoggedInFormGoggle = false;
+const LoggedInFormEmail  = true;
+if (userLoggedIn && debitCard && 2 == 2) {
+    console.log("Allow to by courses");
     
-// }
-// if (LoggedInFormGoggle || LoggedInFormEmail) {
-//     console.log("user Logged In");
+}
+if (LoggedInFormGoggle || LoggedInFormEmail) {
+    console.log("user Logged In");
     
 
-// }
+}
 
 
 
 /////////////// Switch ///////////////
 
 
-// switch (key) {
-//     case value:
+switch (key) {
+    case value:
         
-//         break;
+        break;
 
-//     default:
-//         break;
-// }
+    default:
+        break;
+}
 
 
 // const month = "March";
@@ -86,11 +86,11 @@ const balance  = 1000;
 //         break;
 
 
-//     default:
-//         console.log("default case match");
+    default:
+        console.log("default case match");
         
-//         break;
-// }
+        break;
+}
 
 
 ////////  truthy   //////////
