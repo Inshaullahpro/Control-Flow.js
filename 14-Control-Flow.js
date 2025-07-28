a734941ad7e0c89873ba21b946cbe062cc983201
Inshaullahpro <kundiinshaullah3@gmail.com>
2025-07-29 00:34:10 +0500

    Update 14-Control-Flow.js

--- 14-Control-Flow.js
+++ 14-Control-Flow.js
@@ -4,13 +4,15 @@
  const temperature = 41;
 
  if  (temperature === 50) {
-    console.log("less than 50");
+    // console.log("less than 50");
+  console.log("less than 50");
     
  } else {
     console.log("temperature is greater is than 50");
     
  }
- console.log("execute");
+ // console.log("execute");
+console.log("execute");
  
 
 
